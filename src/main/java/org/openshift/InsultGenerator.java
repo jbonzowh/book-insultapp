4dbc0426e75e05ac8f03a28c4e02af88dff7e22f
Jesse Bonzo <jbonzo@williamhill.us>
2019-10-30 16:32:20 -0700

    Using a database

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -6,28 +6,25 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class InsultGenerator {
+    private final String databaseURL = "jdbc:postgresql://" + System.getenv("POSTGRESQL_SERVICE_HOST") + "/"
+            + System.getenv("POSTGRESQL_DATABASE");
+    private final String username = System.getenv("POSTGRESQL_USER");
+    private final String password = System.getenv("PGPASSWORD");
+    private final String vowels = "AEIOU";
+
     public String generateInsult() {
-        String vowels = "AEIOU";
-        String article = "an";
-        String theInsult = "";
-        try {
-            String databaseURL = "jdbc:postgresql://";
-            databaseURL += System.getenv("POSTGRESQL_SERVICE_HOST");
-            databaseURL += "/" + System.getenv("POSTGRESQL_DATABASE");
-            String username = System.getenv("POSTGRESQL_USER");
-            String password = System.getenv("PGPASSWORD");
-            try (Connection connection = DriverManager.getConnection(databaseURL, username, password)) {
-                if (connection != null) {
-                    String SQL =
-                            "select a.string AS first, b.string AS second, c.string AS noun    from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
-                    try (Statement stmt = connection.createStatement(); ResultSet rs = stmt.executeQuery(SQL)) {
-                        while (rs.next()) {
-                            if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
-                                article = "a";
-                            }
-                            theInsult = String.format("Thou art %s %s %s %s!", article, rs.getString("first"),
-                                    rs.getString("second"), rs.getString("noun"));
+        try (Connection connection = DriverManager.getConnection(databaseURL, username, password)) {
+            if (connection != null) {
+                String SQL =
+                        "select a.string AS first, b.string AS second, c.string AS noun    from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
+                try (Statement stmt = connection.createStatement(); ResultSet rs = stmt.executeQuery(SQL)) {
+                    while (rs.next()) {
+                        String article = "an";
+                        if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
+                            article = "a";
                         }
+                        return String.format("Thou art %s %s %s %s!", article, rs.getString("first"),
+                                rs.getString("second"), rs.getString("noun"));
                     }
                 }
             }
@@ -35,6 +32,6 @@ public class InsultGenerator {
             e.printStackTrace();
             return "Database connection problem!";
         }
-        return theInsult;
+        return "";
     }
 }
